6f50dc235fcd9f54987ab15d157dafea260e8675
Rustem <63965587+rustem-garr@users.noreply.github.com>
2023-05-18 17:27:39 +0500

    Update modal-window.js

--- static/js/modal-window.js
+++ static/js/modal-window.js
@@ -3,12 +3,18 @@
 
     // Get the button that opens the modal
     var btn = document.getElementById("btnContact");
+    var btn1 = document.getElementById("btnContact1");
 
     // Get the <span> element that closes the modal
     var span = document.getElementsByClassName("close")[0];
 
     // When the user clicks the button, open the modal
     btn.onclick = function () {
+      modal.style.display = "block";
+    };
+
+    btn1.onclick = function () {
+      document.getElementById("side-menu").style.width = "0";
       modal.style.display = "block";
     };
 
