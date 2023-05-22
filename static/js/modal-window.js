9111c5174ff3e2307785ccf1fb2b336eed25c7ed
Rustem <63965587+rustem-garr@users.noreply.github.com>
2023-05-22 20:55:15 +0500

    Update modal-window.js

--- static/js/modal-window.js
+++ static/js/modal-window.js
@@ -26,6 +26,6 @@
     // When the user clicks anywhere outside of the modal, close it
     window.onclick = function (event) {
       if (event.target == modal) {
-        modal.style.display = "none";
+        modal3.style.display = "none";
       }
     };
