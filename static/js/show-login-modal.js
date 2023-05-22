b4c49761ff5c927b6d9aafddfea4f21fdb25ff28
Rustem <63965587+rustem-garr@users.noreply.github.com>
2023-05-22 19:56:04 +0500

    Create show-login-modal.js

---
+++ static/js/show-login-modal.js
@@ -0,0 +1,7 @@
+var btn = document.getElementById("btn-login-modal")
+var modal=document.getElementById("id01")
+
+btn.onclick = function () {
+      document.getElementById("side-menu").style.width = "0";
+      modal.style.display = "block";
+    };
