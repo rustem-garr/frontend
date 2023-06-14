378207b7ed36148899742388406e6b88c63f095a
Rustem <63965587+rustem-garr@users.noreply.github.com>
2023-06-14 16:40:44 +0500

    Update banner-slide.js

--- static/js/banner-slide.js
+++ static/js/banner-slide.js
@@ -2,6 +2,7 @@ let slideIndex = 0;
 showSlides();
 
 function showSlides() {
+  document.getElementById('banner-hidden').style.display = 'none';
   let i;
   let slides = document.getElementsByClassName("mySlides");
   for (i = 0; i < slides.length; i++) {
@@ -10,36 +11,5 @@ function showSlides() {
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}    
   slides[slideIndex-1].style.display = "block"; 
-  setTimeout(showSlides, 6000); // Change image every 6 seconds
+  setTimeout(showSlides, 5000); // Change image every 6 seconds
 }
-
-
-// var i = 0;
-// var images = [];
-// var slideTime = 3000; // 3 seconds
-
-// images[0] = '../images/banner1.jpg';
-// images[1] = '../images/banner2.jpg';
-// images[2] = '../images/banner3.jpg';
-
-// function changePicture() {
-//   var w = window.innerWidth;
-//   if (w >= 750) {
-//     var img = document.getElementById('cont-img')
-//     img.style.backgroundImage="url(" + images[i] + ")";
-//   }
-//   else {
-//     var img=document.getElementById('banner-img')
-//   }
-
-//     img.style.backgroundImage = "url(" + images[i] + ")";
-
-//     if (i < images.length - 1) {
-//         i++;
-//     } else {
-//         i = 0;
-//     }
-//     setTimeout(changePicture, slideTime);
-// }
-
-// window.onload = changePicture;
