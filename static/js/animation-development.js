b75a1e224a8c0c5f236c9f778bc57c7d3f2da024
Rustem <63965587+rustem-garr@users.noreply.github.com>
2023-06-14 14:16:29 +0500

    Update animation-development.js

--- static/js/animation-development.js
+++ static/js/animation-development.js
@@ -9,11 +9,9 @@ const observer = new IntersectionObserver(
 });
 
 const divPartners = document.querySelectorAll('.history-card')
-const searchParams = document.querySelectorAll('.search-params')
 const footer = document.querySelectorAll('.footer')
 const footerSocial = document.querySelectorAll('.footer-social')
 
 divPartners.forEach((el) => observer.observe(el));
-searchParams.forEach((el1) => observer.observe(el1));
 footer.forEach((el3) => observer.observe(el3));
 footerSocial.forEach((el4) => observer.observe(el4));
