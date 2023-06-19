d59ccd39f604e4c402d38409e68a11702bfb7c10
Rustem <63965587+rustem-garr@users.noreply.github.com>
2023-06-19 19:39:37 +0500

    Update animation-about-us.js

--- static/js/animation-about-us.js
+++ static/js/animation-about-us.js
@@ -10,10 +10,8 @@ const observer = new IntersectionObserver(
 
 const divPartners = document.querySelectorAll('.history-card')
 const allCertificates = document.querySelectorAll('.all-certificates')
-const footer = document.querySelectorAll('.footer')
 const footerSocial = document.querySelectorAll('.footer-social')
 
 divPartners.forEach((el) => observer.observe(el));
 allCertificates.forEach((el1) => observer.observe(el1));
-footer.forEach((el3) => observer.observe(el3));
 footerSocial.forEach((el4) => observer.observe(el4));
