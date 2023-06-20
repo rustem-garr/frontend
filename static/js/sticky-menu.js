28a2454432bc30d771b033e6f851f675206e69c1
Rustem <63965587+rustem-garr@users.noreply.github.com>
2023-06-20 20:10:42 +0500

    Update sticky-menu.js

--- static/js/sticky-menu.js
+++ static/js/sticky-menu.js
@@ -3,9 +3,9 @@ myID = document.getElementById("stickyMenu");
 var myScrollFunc = function() {
   var y = window.scrollY;
   if (y >= 650) {
-    myID.className = "bottomMenu show"
+    myID.className = "bottomMenu1 show"
   } else {
-    myID.className = "bottomMenu hide"
+    myID.className = "bottomMenu1 hide"
   }
 };
 
